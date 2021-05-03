d252bbc2f432a5f922df157c81ae7f2cb3b36f53
MDSergeev <jong8@mail.ru>
2021-05-03 09:16:52 +0300

    Методы протестированы на большем количество ограничений

--- main.cpp
+++ main.cpp
@@ -8,12 +8,20 @@
 #include "OriginalIndexMethod.h"
 #include "GridSearchMethod.h"
 
-double f1(double x) {
+double f0(double x) {
 	return exp(-0.5 * x) * sin(6 * x - 1.5);
 }
 
-double f2(double x) {
+double f1(double x) {
 	return fabs(x) * sin(2 * M_PI * x - 0.5);
+}
+
+double f2(double x) {
+	return sin(2 * M_PI * x) * sqrt(fabs(x));
+}
+
+double f3(double x) {
+	return (sqrt(fabs(x)) + 2 * cos(M_PI * x)) / (2 + sin(2 * M_PI * x));
 }
 
 double fi(double x) {
@@ -37,26 +45,23 @@ int main() {
 	std::cout << "Param R = ";
 	std::cin >> r;
 
-	std::vector<std::function<double(double)>> funcs;
-	funcs.push_back(f1);
-	funcs.push_back(f2);
-	funcs.push_back(fi);
+	Funcs funcs{f0, f1, f2, f3, fi};
 	
 	OriginalIndexMethod im(a, b, eps, r, funcs);
 	PointTrial imBestTrial = im.Run();
 	std::cout << "BEST TRIAL INDEX METHOD" << std::endl;
 	std::cout << std:: fixed << std::setprecision(5)
-		<< "x = " << imBestTrial.getX() << " | "
-		<< "value = " << imBestTrial.getValue() << " | "
-		<< "index = " << imBestTrial.getIndex() << " |" << std:: endl;
+		<< "x = " << imBestTrial.x() << " | "
+		<< "value = " << imBestTrial.value() << " | "
+		<< "index = " << imBestTrial.index() << " |" << std:: endl;
 	
 	GridSearchMethod gsm(a, b, eps, funcs);
 	PointTrial gsmBestTrial = gsm.Run();
 	std::cout << "BEST TRIAL GRID SEARCH METHOD" << std::endl;
 	std::cout << std:: fixed << std::setprecision(5)
-		<< "x = " << gsmBestTrial.getX() << " | "
-		<< "value = " << gsmBestTrial.getValue() << " | "
-		<< "index = " << gsmBestTrial.getIndex() << " |" << std::endl;
+		<< "x = " << gsmBestTrial.x() << " | "
+		<< "value = " << gsmBestTrial.value() << " | "
+		<< "index = " << gsmBestTrial.index() << " |" << std::endl;
 	
 	return 0;
 }
